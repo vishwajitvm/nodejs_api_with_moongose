152d23c9431333176b03e5c3ae21c0080e5e0a60
vishwajitvm <vishwajitmall50@gmail.com>
2023-05-14 15:04:43 +0530

    get api

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const app = express() ;
 app.use(express.json())
 
 //####################INSERT ROUTE################
-//####################INSERT ROUTE################
 app.post('/create' , async(req , resp) => {
     let data = new product(req.body) ;
     let result = await data.save() ;
     console.log(result);
     resp.send(result) ;
 })
+
+//####################GET ROUTE################
+app.get('/list' , async (req , resp) => {
+    let data = await product.find() ;
+    console.log(data) ;
+    resp.send(data) ;
+})
+
 
 app.listen(port) ;
